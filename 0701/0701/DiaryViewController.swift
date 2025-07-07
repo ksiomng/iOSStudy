ee36d96761d2dad26cf431990824b98caf565761
Song Kim <songkim@Songui-MacBookPro-2.local>
2025-07-07 10:26:13 +0900

    Fix: 0701 과제 업데이트

--- 0701/0701/DiaryViewController.swift
+++ 0701/0701/DiaryViewController.swift
@@ -9,147 +9,39 @@ import UIKit
 
 class DiaryViewController: UIViewController {
     var dic: [String: Int] = [:]
+    var arr = ["행복해", "사랑해", "좋아해", "당황해", "속상해", "우울해", "심심해", "따분해", "지루해"]
     
-    @IBOutlet var button1: UIButton!
-    @IBOutlet var label1: UILabel!
-    @IBOutlet var button2: UIButton!
-    @IBOutlet var label2: UILabel!
-    @IBOutlet var button3: UIButton!
-    @IBOutlet var label3: UILabel!
-    @IBOutlet var button4: UIButton!
-    @IBOutlet var label4: UILabel!
-    @IBOutlet var button5: UIButton!
-    @IBOutlet var label5: UILabel!
-    @IBOutlet var button6: UIButton!
-    @IBOutlet var label6: UILabel!
-    @IBOutlet var button7: UIButton!
-    @IBOutlet var label7: UILabel!
-    @IBOutlet var button8: UIButton!
-    @IBOutlet var label8: UILabel!
-    @IBOutlet var button9: UIButton!
-    @IBOutlet var label9: UILabel!
+    @IBOutlet var buttons: [UIButton]!
+    @IBOutlet var labels: [UILabel]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemGray6
-        setImageUI()
-        setDic()
-    }
-    func setImageUI() {
-        button1.setImage(UIImage(named: "mono_slime1"), for: .normal)
-        button1.setTitle("", for: .normal)
         
-        button2.setImage(UIImage(named: "mono_slime2"), for: .normal)
-        button2.setTitle("", for: .normal)
+        for a in arr {
+            dic[a] = 0
+        }
         
-        button3.setImage(UIImage(named: "mono_slime3"), for: .normal)
-        button3.setTitle("", for: .normal)
-        
-        button4.setImage(UIImage(named: "mono_slime4"), for: .normal)
-        button4.setTitle("", for: .normal)
-        
-        button5.setImage(UIImage(named: "mono_slime5"), for: .normal)
-        button5.setTitle("", for: .normal)
-        
-        button6.setImage(UIImage(named: "mono_slime6"), for: .normal)
-        button6.setTitle("", for: .normal)
-        
-        button7.setImage(UIImage(named: "mono_slime7"), for: .normal)
-        button7.setTitle("", for: .normal)
-        
-        button8.setImage(UIImage(named: "mono_slime8"), for: .normal)
-        button8.setTitle("", for: .normal)
-        
-        button9.setImage(UIImage(named: "mono_slime9"), for: .normal)
-        button9.setTitle("", for: .normal)
+        for i in 0..<9 {
+            setImageUI(btn: buttons[i], idx: i)
+            setLabelUI(lbl: labels[i], idx: i)
+        }
     }
     
-    func setDic() {
-        dic["행복해"] = 0
-        dic["사랑해"] = 0
-        dic["좋아해"] = 0
-        dic["당황해"] = 0
-        dic["속상해"] = 0
-        dic["우울해"] = 0
-        dic["심심해"] = 0
-        dic["따분해"] = 0
-        dic["지루해"] = 0
-        
-        setLabelUI()
+    func setImageUI(btn: UIButton, idx: Int) {
+        btn.setImage(UIImage(named: "mono_slime\(idx+1)"), for: .normal)
+        btn.setTitle("\(idx)", for: .normal)
     }
     
-    func setLabelUI() {
-        label1.text = "행복해 \(dic["행복해"]!)"
-        label1.textAlignment = .center
-        
-        label2.text = "사랑해 \(dic["사랑해"]!)"
-        label2.textAlignment = .center
-        
-        label3.text = "좋아해 \(dic["좋아해"]!)"
-        label3.textAlignment = .center
-        
-        label4.text = "당황해 \(dic["당황해"]!)"
-        label4.textAlignment = .center
-        
-        label5.text = "속상해 \(dic["속상해"]!)"
-        label5.textAlignment = .center
-        
-        label6.text = "우울해 \(dic["우울해"]!)"
-        label6.textAlignment = .center
-        
-        label7.text = "심심해 \(dic["심심해"]!)"
-        label7.textAlignment = .center
-        
-        label8.text = "따분해 \(dic["따분해"]!)"
-        label8.textAlignment = .center
-        
-        label9.text = "지루해 \(dic["지루해"]!)"
-        label9.textAlignment = .center
-    }
-
-    @IBAction func tapButton1(_ sender: Any) {
-        dic["행복해"]! += 1
-        setLabelUI()
+    func setLabelUI(lbl: UILabel, idx: Int) {
+        let str = arr[idx]
+        lbl.text = "\(str) \(dic[str]!)"
+        lbl.textAlignment = .center
     }
     
-    @IBAction func tapButton2(_ sender: Any) {
-        dic["사랑해"]! += 1
-        setLabelUI()
+    @IBAction func tapButton(_ sender: UIButton) {
+        let idx = Int(sender.currentTitle ?? "0")!
+        dic[arr[idx]]! += 1
+        setLabelUI(lbl: labels[idx], idx: idx)
     }
-    
-    @IBAction func tapButton3(_ sender: Any) {
-        dic["좋아해"]! += 1
-        setLabelUI()
-    }
-    
-    @IBAction func tapButton4(_ sender: Any) {
-        dic["당황해"]! += 1
-        setLabelUI()
-    }
-    
-    @IBAction func tapButton5(_ sender: Any) {
-        dic["속상해"]! += 1
-        setLabelUI()
-    }
-   
-    @IBAction func tapButton6(_ sender: Any) {
-        dic["우울해"]! += 1
-        setLabelUI()
-    }
-    
-    @IBAction func tapButton7(_ sender: Any) {
-        dic["심심해"]! += 1
-        setLabelUI()
-    }
-    
-    @IBAction func tapButton8(_ sender: Any) {
-        dic["따분해"]! += 1
-        setLabelUI()
-    }
-    
-    @IBAction func tapButton9(_ sender: Any) {
-        dic["지루해"]! += 1
-        setLabelUI()
-    }
-    
 }
