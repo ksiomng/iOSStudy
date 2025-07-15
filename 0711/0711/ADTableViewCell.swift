47124a2c331701c19fbf3253b1fbad003652558b
ksiomng <nadana0929@gmail.com>
2025-07-15 13:37:18 +0900

    Fix: 배경색을 랜덤으로

--- 0711/0711/ADTableViewCell.swift
+++ 0711/0711/ADTableViewCell.swift
@@ -13,12 +13,15 @@ class ADTableViewCell: UITableViewCell {
     @IBOutlet var ADWhiteLabel: UILabel!
     @IBOutlet var contentLabel: UILabel!
     
+    let randomColor: [UIColor] = [.lightPink, .yellow, .cyan]
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         backgroundColorView.layer.cornerRadius = 10
         ADWhiteLabel.clipsToBounds = true
         ADWhiteLabel.layer.cornerRadius = 15
         contentLabel.font = UIFont.systemFont(ofSize: 20, weight: .semibold)
+        backgroundColorView.backgroundColor = randomColor.randomElement()
     }
 }
 
