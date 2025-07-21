e1539a8be3ea0246e9522c766fe70efcac4037b2
ksiomng <nadana0929@gmail.com>
2025-07-21 23:55:43 +0900

    Feat: 뷰 완성

--- 0721-SnapKit/0721-SnapKit/ViewController.swift
+++ 0721-SnapKit/0721-SnapKit/ViewController.swift
@@ -32,12 +32,55 @@ class ViewController: UIViewController {
         whiteCardView.layer.cornerRadius = 15
         
         whiteCardView.addSubview(topNPayLabel)
+        topNPayLabel.backgroundColor = .yellow
         topNPayLabel.snp.makeConstraints { make in
             make.top.equalTo(whiteCardView).offset(10)
             make.leading.equalTo(whiteCardView).offset(10)
-            make.width.equalTo(100)
+            make.width.equalTo(150)
+            make.height.equalTo(50)
         }
-        topNPayLabel.text = "NPAY국내"
+        
+        cancelButton.backgroundColor = .blue
+        whiteCardView.addSubview(cancelButton)
+        cancelButton.snp.makeConstraints { make in
+            make.top.equalTo(whiteCardView).offset(10)
+            make.trailing.equalTo(whiteCardView).inset(10)
+            make.width.height.equalTo(50)
+        }
+        
+        messageLabel.backgroundColor = .cyan
+        whiteCardView.addSubview(messageLabel)
+        messageLabel.snp.makeConstraints { make in
+            make.leading.trailing.equalTo(whiteCardView).inset(10)
+            make.top.equalTo(topNPayLabel.snp.bottom).offset(50)
+            make.height.equalTo(150)
+        }
+        
+        checkedButton.backgroundColor = .red
+        whiteCardView.addSubview(checkedButton)
+        checkedButton.snp.makeConstraints { make in
+            make.top.equalTo(messageLabel.snp.bottom).offset(50)
+            make.leading.equalTo(whiteCardView).offset(80)
+            make.width.height.equalTo(30)
+        }
+        
+        checkedButtonLabel.backgroundColor = .red
+        whiteCardView.addSubview(checkedButtonLabel)
+        checkedButtonLabel.snp.makeConstraints { make in
+            make.centerY.equalTo(checkedButton)
+            make.leading.equalTo(checkedButton.snp.trailing).offset(20)
+            make.trailing.equalTo(whiteCardView).offset(-80)
+            make.height.equalTo(30)
+        }
+        
+        resultButton.backgroundColor = .gray
+        whiteCardView.addSubview(resultButton)
+        resultButton.snp.makeConstraints { make in
+            make.bottom.equalTo(whiteCardView).offset(-15)
+            make.leading.trailing.equalTo(whiteCardView).inset(30)
+            make.height.equalTo(50)
+        }
+        resultButton.layer.cornerRadius = 15
     }
 
 
