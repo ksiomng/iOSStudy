b3aeb31433801a93a5757ab7b9e823cb80464de6
ksiomng <nadana0929@gmail.com>
2025-07-26 23:23:51 +0900

    Feat: 데이터모델 구조

---
+++ 0725-Shopping/0725-Shopping/Shop.swift
@@ -0,0 +1,19 @@
+//
+//  Shop.swift
+//  0725-Shopping
+//
+//  Created by Song Kim on 7/26/25.
+//
+
+import Foundation
+
+struct Shops: Decodable {
+    let items: [Shop]
+}
+
+struct Shop: Decodable {
+    let image: String
+    let brand: String
+    let title: String
+    let lprice: Int
+}
