9479c1444546d4bc2e95dd1d2e73fb6747fb1f46
ksiomng <nadana0929@gmail.com>
2025-07-30 20:41:22 +0900

    Feat: BaseView 써보기 위해서 세팅

---
+++ 0725-Shopping/0725-Shopping/Base/BaseTableViewCell.swift
@@ -0,0 +1,35 @@
+//
+//  BaseTableViewCell.swift
+//  0725-Shopping
+//
+//  Created by Song Kim on 7/30/25.
+//
+
+import UIKit
+
+class BaseTableViewCell: UITableViewCell {
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+            super.init(style: style, reuseIdentifier: reuseIdentifier)
+            
+            configureHierarchy()
+            configureLayout()
+            configureView()
+        }
+        
+        func configureHierarchy() {
+            
+        }
+        
+        func configureLayout() {
+            
+        }
+        
+        func configureView() {
+            
+        }
+        
+        @available(*, unavailable)
+        required init?(coder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+}
---
+++ 0725-Shopping/0725-Shopping/Base/BaseViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BaseViewController.swift
+//  0725-Shopping
+//
+//  Created by Song Kim on 7/30/25.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("base", #function)
+        setupHierarchy()
+        setupLayout()
+        setupView()
+    }
+    
+    func setupHierarchy() {
+        
+    }
+    
+    func setupLayout() {
+        
+    }
+    
+    func setupView() {
+        
+    }
+}
