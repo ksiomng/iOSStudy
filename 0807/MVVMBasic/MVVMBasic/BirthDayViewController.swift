0f955df43617c1110073c96fc424bc35a8e25206
ksiomng <nadana0929@gmail.com>
2025-08-08 01:19:01 +0900

    Feat: 과제 마무리

--- 0807/MVVMBasic/MVVMBasic/BirthDayViewController.swift
+++ 0807/MVVMBasic/MVVMBasic/BirthDayViewController.swift
@@ -8,6 +8,14 @@
 import UIKit
 import SnapKit
 
+enum birthDayError: Error {
+    case emptyString
+    case isNotInt
+    case year
+    case month
+    case day
+}
+
 class BirthDayViewController: UIViewController {
     let yearTextField: UITextField = {
         let textField = UITextField()
@@ -130,5 +138,71 @@ class BirthDayViewController: UIViewController {
     
     @objc func resultButtonTapped() {
         view.endEditing(true)
+        let year = yearTextField.text ?? ""
+        let month = monthTextField.text ?? ""
+        let day = dayTextField.text ?? ""
+        
+        do {
+            let birthDate = try checkBirthDayError(year: year, month: month, day: day)
+            let today = Date()
+            let days = Calendar.current.dateComponents([.day], from: birthDate, to: today).day ?? 0
+            resultLabel.text = "오늘은 \(days)일째입니다"
+        } catch {
+            switch error {
+            case .emptyString:
+                resultLabel.text = "입력된 값이 없습니다"
+            case .isNotInt:
+                resultLabel.text = "숫자를 입력해주세요"
+            case .year:
+                resultLabel.text = "1000년부터 2025년까지 중에 입력해주세요"
+            case .month:
+                resultLabel.text = "1월부터 12월중에 입력해주세요"
+            case .day:
+                resultLabel.text = "월에 맞는 일수를 입력해주세요"
+            }
+        }
+    }
+    
+    func checkBirthDayError(year: String, month: String, day: String) throws(birthDayError) -> Date {
+        guard !(year.isEmpty) && !(month.isEmpty) && !(day.isEmpty) else {
+            throw .emptyString
+        }
+        guard Int(year) != nil && Int(month) != nil && Int(day) != nil else {
+            throw .isNotInt
+        }
+        guard 1000 < Int(year)! && 2026 > Int(year)! else {
+            throw .year
+        }
+        guard 0 < Int(month)! && Int(month)! <= 12 else {
+            throw .month
+        }
+        guard 0 < Int(day)! && Int(day)! < checkDateRange(month: Int(month)!) else {
+            throw .day
+        }
+        return stringToDate(year: year, month: month, day: day)
+    }
+    
+    func checkDateRange(month: Int) -> Int {
+        if month == 4 || month == 6 || month == 9 || month == 11 {
+            return 30
+        } else if month == 2 {
+            return 28
+        } else {
+            return 31
+        }
+    }
+    
+    func stringToDate(year: String, month: String, day: String) -> Date {
+        var dateComponents = DateComponents()
+        dateComponents.year = Int(year)!
+        dateComponents.month = Int(month)!
+        dateComponents.day = Int(day)!
+        
+        let calendar = Calendar.current
+        guard let birthDate = calendar.date(from: dateComponents) else {
+            resultLabel.text = "날짜변환실패"
+            return Date()
+        }
+        return birthDate
     }
 }
