a1e113d9680dd4839ff092284d99d99c25a84408
ksiomng <nadana0929@gmail.com>
2025-08-20 18:56:25 +0900

    Chore: 프로젝트 세팅

---
+++ 0820/0820/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  0820
+//
+//  Created by Song Kim on 8/20/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
