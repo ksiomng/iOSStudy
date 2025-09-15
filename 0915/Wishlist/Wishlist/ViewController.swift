c6ee619fe187278e188523b0d2d9933d4b11b0e3
ksiomng <nadana0929@gmail.com>
2025-09-15 22:36:12 +0900

    Chore: 프로젝트 세팅

---
+++ 0915/Wishlist/Wishlist/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Wishlist
+//
+//  Created by Song Kim on 9/15/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
